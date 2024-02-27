3ba57f63ef8e6e8c52da4225da45e45991dbbef8
Yousaf Nabi <you@saf.dev>
2024-02-27 03:17:15 +0000

    feat: bump to slimmed 0.4.18 binaries

--- script/lib/export-binary-versions.sh
+++ script/lib/export-binary-versions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eu
 LIB_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 PROJECT_DIR="${LIB_DIR}"/../../
-export FFI_VERSION=v0.4.16
-export STANDALONE_VERSION=2.0.3
-export PLUGIN_CLI_VERSION=v0.1.3
+export FFI_VERSION=v0.4.18
+export STANDALONE_VERSION=2.4.1
+export PLUGIN_CLI_VERSION=v0.1.2
 export VERIFIER_CLI_VERSION=v1.0.3
 # export STANDALONE_VERSION=$(grep "PACT_STANDALONE_VERSION = '" "$PROJECT_DIR"/standalone/install.ts | grep -E -o "'(.*)'" | cut -d"'" -f2)
 # export FFI_VERSION=v$(grep "PACT_FFI_VERSION = '" "$PROJECT_DIR"/src/ffi/index.ts | grep -E -o "'(.*)'" | cut -d"'" -f2)
